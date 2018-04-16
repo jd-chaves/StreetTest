e4f05cde7cb0eb97ff8b201472a493e25523eb95
jd-chaves <jd.chaves@uniandes.edu.co>
2018-04-16 08:52:13 -0500

    Update accounts-config.js

--- startup/accounts-config.js
+++ startup/accounts-config.js
@@ -1,2 +1,5 @@
+
+/*Bajo la carpeta startup puede crearse un archivo con el objetivo de configurar el lenguage de la pagina
+para no generar errores en los Audits*/
 import { Accounts } from "meteor/accounts-base";
 
